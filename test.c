1fbc4592f6132f7ff7d832bddf02ccc0f7251d48
Cooper2710 <cooper.gunther@ucas-edu.net>
2024-09-09 17:16:14 +0000

    FIxed python code

--- test.c
+++ test.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(void){
-    printf("hello world");
+    printf(name);
     return 0;
 }
--- test.py
+++ test.py
@@ -1,2 +1,3 @@
 name=input("What is your name?: ")
 print ("hello,",name)
+print ("We have been trying to reach you for a long time about your cars extended warranty")
