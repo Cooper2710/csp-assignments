1006324421cbe5e7e73ed9718e7ece81673afe3f
Cooper2710 <cooper.gunther@ucas-edu.net>
2024-09-10 00:44:44 +0000

    c

--- test.c
+++ test.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
-int main(void){
-    printf(name);
+int main(void) {
+    char name[30];
+    printf("Please tell me your name: \n");
+    fgets(name, sizeof(name), stdin);
+    printf("Hello %s", name);
     return 0;
 }
