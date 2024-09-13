825340b7b65fb31117f28e58bb80591e62b151e7
Cooper2710 <cooper.gunther@ucas-edu.net>
2024-09-13 04:40:37 +0000

    dfghjk

--- Expressions.c
+++ Expressions.c
@@ -4,9 +4,22 @@
 int main(void)
 {
     int a = (7-4/8*4+6);
-    printf("%d", a);
     int b = (18/3-7+2*5);
-    printf("%d", b);
-
+    int c = (6*4/12+72/8-9);
+    int d = ((17-6/2)+4*3);
+    int e = (-2*(1*4-2/2)+(6+2-3));
+    int f = (-1*((3-4*7)/5)-2*24/6);
+    int g = (3*(5*5)/15)-(5-(2*2));
+    int h = (((1*1*1*1)*(2*2)+(3*3*3))-(2*2*2*2*2)/4);
+    int i = (((22/2-2*5)*(22/2-2*5))+((4-6/6)*(4-6/6)));
+    printf("%d \n", a);
+    printf("%d \n", b);
+    printf("%d \n", c);
+    printf("%d \n", d);
+    printf("%d \n", e);
+    printf("%d \n", f);
+    printf("%d \n", g);
+    printf("%d \n", h);
+    printf("%d \n", i);
     return 0;
 }
