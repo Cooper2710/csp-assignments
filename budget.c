ad7d40c999842c7ed90d3c27c46f18103162c66c
Cooper2710 <cooper.gunther@ucas-edu.net>
2024-09-13 19:18:56 +0000

    e

--- budget.c
+++ budget.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
 
 int main(void){
-    char income[9];
-    char utilities[9];
-    char groceries[9];
-    char car[9];
+    float income, rent, utilities, groceries, transportation, savings, spent, expenses;
+    float prent, pgroceries, ptransportation, psavings, pexpenses, putilities;
     printf("this is a budget calculator.\n How much money do you make a month? \n");
-    scanf("%s", income);
+    scanf("%f", &income);
     printf("How much do your utilities cost? \n");
-    scanf("%s", utilities);
+    scanf("%f", &utilities);
     printf("How much does your groceries cost? \n");
-    scanf("%s", income);
-    printf("you make %s\n", income);
-    float spend[] = (float)utilities;
-    
+    scanf("%f", &groceries);
+    printf("How much do your transportation cost? \n");
+    scanf("%f", &transportation);
+    printf("How much do your rent cost? \n");
+    scanf("%f", &rent);
+    savings = income *.2;
+    expenses = rent + utilities + groceries + transportation;
+    spent = income - expenses - savings;
+    printf("you make %.2f\n", income);
+    printf("your expenses are $%.2f\n", expenses);
+    printf("your savings are $%.2f\n", savings);
+    printf("your spending money is $%.2f\n", spent);
+    prent = rent/income * 100;
+    pgroceries = groceries/income * 100;
+    ptransportation = transportation/income * 100;
+    psavings = savings/income * 100;
+    pexpenses = expenses/income * 100;
+    printf("your rent is %d%% of your income.\n", (int) prent);
+    printf("your utilities are %d%% of your income.\n", (int) putilities);
     return 0;
 }
