844972c94835ec18ebd5e7fd935ae8c7f9e9cd02
Cooper2710 <cooper.gunther@ucas-edu.net>
2024-09-13 04:05:12 +0000

    hf

--- Expressions.c
+++ Expressions.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <math.h>
 
-int main() {
-    
+int main(void)
+{
+    int a = (7-4/8*4+6);
+    printf("%d", a);
+    int b = (18/3-7+2*5);
+    printf("%d", b);
+
     return 0;
 }
