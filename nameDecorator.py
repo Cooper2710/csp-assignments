1090ad5cebc23eb89b8d58b4a8c69056cc59dab7
Cooper2710 <cooper.gunther@ucas-edu.net>
2024-09-18 18:30:26 +0000

    pain

---
+++ nameDecorator.py
@@ -0,0 +1,4 @@
+print("hello, this is a name decorator")
+name=input("what is your name?:")
+decor=input("What would you like your name decoration to be? You may put several characters:")
+print(decor,name,decor)
