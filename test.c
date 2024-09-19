682d7f37957a464b8e762c26e21e3041fb8c52dc
Cooper2710 <cooper.gunther@ucas-edu.net>
2024-09-19 19:43:31 +0000

    dfgh

--- test.c
+++ test.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 
 int main(void){
-    char name[30];
-    printf("Please tell me your name: \n");
-    fgets(name, sizeof(name), stdin);
-    printf("Hello %s", name);
+    
     return 0;
 }
