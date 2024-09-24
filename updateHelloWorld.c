70e72b889b1b4b6c164d43fb97b7e8ca75d34793
Cooper2710 <cooper.gunther@ucas-edu.net>
2024-09-24 19:32:05 +0000

    paain

---
+++ updateHelloWorld.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void name(char name[]){
+printf("Hello %s\n", name);
+}
+
+int main(void){
+    name("stoopid");
+    name("muhammed");
+    name("darius");
+    name("john smith");
+    name("linleigh");
+    return 0;
+}
---
+++ updateHelloWorld.py
@@ -0,0 +1,8 @@
+def name(name):
+    return f"Hello {name}"
+
+print(name("john"))
+print(name("chickenleigh"))
+print(name("muhammed"))
+print(name("ahmed"))
+print(name("lincoln"))
