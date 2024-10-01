1562c2443cc87a803ffdad526beffa31d65abc25
Cooper2710 <cooper.gunther@ucas-edu.net>
2024-10-01 17:09:10 +0000

    234

---
+++ oldEnough.py
@@ -0,0 +1,25 @@
+age=14
+if (age >= 18):{
+    print("you are old enough to vote")
+}
+else:{
+    print("you cant vote")
+}
+if (age >= 16):{
+    print("you can drive")
+}
+else:{
+    print("you cant drive")
+}
+if (age >= 15):{
+    print("you can get a learners permit")
+}
+else:{
+    print("you cant get a learners permit")
+}
+if (age >= 5):{
+    print("you are old enough to go to school")
+}
+else:{
+    print("you cannot go to school")
+}
