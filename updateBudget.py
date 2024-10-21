ef3b80c379f33b9a12ed9dc88474f48bdd2e048e
Cooper2710 <cooper.gunther@ucas-edu.net>
2024-10-21 19:42:41 +0000

    rooerogfoivc

--- updateBudget.py
+++ updateBudget.py
@@ -1,9 +1,15 @@
 print("This is a finacial calculator")
 
+income=input("what is your monthly income?:\n")
+
 def user_inputs(costs):
-    fees=input("what does your %s cost", costs)
-    return fees
+    fees=input(f"what does your {costs} cost?:\n")
+    return int(fees)
 rent=user_inputs("rent")
+groceries=user_inputs("groceries")
+utilities=user_inputs("utilities")
+transp=user_inputs("transportation")
+
     
 
 
@@ -17,11 +23,11 @@ print("your spending money is", spending)
 
 def percent(type, ammount):
     per = ammount/income *100
-    
-    print(f"your {type} is {per}% income")
+    return(f"your {type} is {per}% income")
 
-percent("rent", rent)
-percent("utilities", utilities)
-percent("groceries", groceries)
-percent("transportation", transp)
-percent("expenses", expenses)
+
+print(percent("rent", rent))
+print(percent("utilities", utilities))
+print(percent("groceries", groceries))
+print(percent("transportation", transp))
+print(percent("expenses", expenses))
